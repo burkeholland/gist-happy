9db0044e960ca899a3c546ed390622fab0dc4f8e
aprilla <aprilla@github.com>
2022-05-06 19:37:05 -0700

    first pass command extraction

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import { registerHelloWorld } from "./commands/hello-world";
+
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Activating ... gist-happy ...!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('gist-happy.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Get started with Gist Happy!');
-	});
-
-	context.subscriptions.push(disposable);
+	registerHelloWorld(context);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
